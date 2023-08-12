a0e6eaa7b8212b69755bfcea1a6bf3c904c688cc
Diwan <diwan.muhamad@nawadata.id>
2023-08-12 08:25:30 +0700

    initialize project

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("hello world!!!");
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcImhlbGxvIHdvcmxkISEhXCIpIl0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7QUFBQUEsUUFBUUMsR0FBRyxDQUFDIn0=
